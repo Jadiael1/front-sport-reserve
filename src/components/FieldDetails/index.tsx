ae85c93a55e016ccaa161fd837c8774bf5158697
Jadiael1 <jadiael@hotmail.com.br>
2024-06-28 14:36:04 -0300

    feat(FieldDetails): add error and success handling with Alert

--- src/components/FieldDetails/index.tsx
+++ src/components/FieldDetails/index.tsx
@@ -1,32 +1,41 @@
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { FaFutbol, FaMapMarkerAlt, FaDollarSign, FaClock } from 'react-icons/fa';
 import { useState } from 'react';
+import { useAuth } from '../../contexts/AuthContext';
+import Alert from '../Alert';
 
 const FieldDetails = () => {
 	const location = useLocation();
-	const navigate = useNavigate();
 	const { field } = location.state;
 	const [startTime, setStartTime] = useState('');
 	const [endTime, setEndTime] = useState('');
+	const { token } = useAuth();
+	const [error, setError] = useState<{ message: string; errors?: { [key: string]: string[] } } | null>(null);
+	const [success, setSuccess] = useState<{ message: string } | null>(null);
 
 	const handleReservation = () => {
+		const startTimeTreated = new Date(startTime).toISOString().replace('T', ' ').slice(0, 19);
+		const endTimeTreated = new Date(endTime).toISOString().replace('T', ' ').slice(0, 19);
 		fetch('https://api-sport-reserve.juvhost.com/api/v1/reservations', {
 			method: 'POST',
 			headers: {
+				Accept: 'application/json',
 				'Content-Type': 'application/json',
-				Authorization: 'Bearer 55|WW6tLdE3GTZ7P5NAgIeUasGtlwmryhlW4dp6sClieccc8044',
+				Authorization: `Bearer ${token}`,
 			},
 			body: JSON.stringify({
 				field_id: field.id,
-				start_time: new Date(startTime).toISOString(),
-				end_time: new Date(endTime).toISOString(),
+				start_time: startTimeTreated,
+				end_time: endTimeTreated,
 			}),
 		})
 			.then(resp => resp.json())
 			.then(data => {
-				console.log('Reservation success:', data);
-				alert('Reserva realizada com sucesso!');
-				navigate('/');
+				if (data.status === 'success') {
+					setSuccess({ message: data.message });
+				} else {
+					setError({ message: data.message, errors: data.errors });
+				}
 			})
 			.catch(error => {
 				console.error('Error:', error);
@@ -36,6 +45,22 @@ const FieldDetails = () => {
 
 	return (
 		<div className='container mx-auto p-4'>
+			{success && (
+				<Alert
+					message={success.message}
+					onClose={() => setError(null)}
+					type='success'
+					redirectTo='/'
+				/>
+			)}
+			{error && (
+				<Alert
+					message={error.message}
+					errors={error.errors}
+					onClose={() => setError(null)}
+					type='error'
+				/>
+			)}
 			<div className='bg-white p-6 rounded-lg shadow-md'>
 				<h1 className='text-3xl font-bold mb-4'>{field.name}</h1>
 				<div className='text-gray-700 mb-2 flex items-center'>
