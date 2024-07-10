8100ec0c6118a6b83eba718d4102e2d17ed9e7fe
Wilksonflor <wilksonflor12@gmail.com>
2024-07-10 17:57:52 -0300

    feat(home,signin): Add responsive menu and loading state

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,16 +1,19 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { IApiResponse, IField } from './interfaces/IFields';
 import { FaFutbol, FaMapMarkerAlt, FaDollarSign } from 'react-icons/fa';
 import { useAuth } from '../../contexts/AuthContext';
 import { CiLogin } from 'react-icons/ci';
-import { IoMdFootball } from 'react-icons/io';
+import { IoMdFootball, IoMdMenu, IoIosLogOut } from 'react-icons/io';
+import { FaRegListAlt } from 'react-icons/fa';
 
 const HomePage = () => {
 	const { user, logout, isLoading } = useAuth();
 	const [responseFields, setResponseFields] = useState<IApiResponse | null>(null);
+	const [menuOpen, setMenuOpen] = useState(false);
 	const baseURL = import.meta.env.VITE_API_BASE_URL;
 	const navigate = useNavigate();
+
 	useEffect(() => {
 		fetch(`${baseURL}/fields`, {
 			method: 'GET',
@@ -36,25 +39,56 @@ const HomePage = () => {
 	};
 
 	return (
-		<div className='mx-auto p-4 w-full bg-background h-lvh'>
+		<section className='mx-auto p-4 w-full bg-background min-h-screen'>
 			{!isLoading && user ?
-				<div>
+				<div className='flex items-center justify-end my-5 md:mx-10'>
 					<button
-						className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 mr-2'
-						onClick={handleGoReservationList}
+						className='block md:hidden bg-gray-200 px-3 py-2 rounded bg-transparent'
+						onClick={() => setMenuOpen(!menuOpen)}
 					>
-						Ver minhas reservas
+						<IoMdMenu className='text-3xl' />
 					</button>
-					<button
-						className='bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 transition duration-300'
-						onClick={logout}
-					>
-						Logout
-					</button>
+					{/* Menu visível em telas menores */}
+					{menuOpen && (
+						// Div do menu responsivo
+						<div className='absolute right-0 h-1/4 top-9 mt-8  bg-white w-full  md:max-w-md lg:max-w-lg shadow-md rounded-md py-2 z-50 '>
+							<button
+								className='block px-4 py-2 text-gray-800 hover:bg-gray-200 w-full flex items-center gap-3 bg-slate-300 font-bold'
+								onClick={handleGoReservationList}
+							>
+								<FaRegListAlt className='text-base' />
+								Ver minhas reservas
+							</button>
+							<button
+								className=' mt-1 px-4 py-2 text-gray-800 hover:bg-gray-200 w-full flex items-center gap-3 bg-white bg-slate-300 font-bold'
+								onClick={logout}
+							>
+								<IoIosLogOut className='text-base' />
+								Logout
+							</button>
+						</div>
+					)}
+					{/* Botões para telas maiores */}
+					<div className='hidden md:flex items-center gap-5'>
+						<button
+							className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 mr-2 flex items-center gap-3'
+							onClick={handleGoReservationList}
+						>
+							<FaRegListAlt className='text-base' />
+							Ver minhas reservas
+						</button>
+						<button
+							className='bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 transition duration-300 flex items-center gap-3'
+							onClick={logout}
+						>
+							<IoIosLogOut className='text-base color-white text-lg' />
+							Sair
+						</button>
+					</div>
 				</div>
-			:	<div className='flex justify-end mb-4'>
+			:	<div className='flex justify-end mb-4 mt-5 md:mt-5'>
 					<button
-						className='flex items-center gap-3 bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-500 transition duration-300 mr-16 mt-3'
+						className='flex items-center gap-3 bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-500 transition duration-300 md:mr-16 '
 						onClick={handleGoSignIn}
 					>
 						<CiLogin />
@@ -75,18 +109,19 @@ const HomePage = () => {
 				<p className='text-lg text-text mt-5'>Alugue nossas quadras esportivas com facilidade.</p>
 			</div>
 
+			{/* Conteúdo dos campos */}
 			{responseFields ?
-				<div className='flex items-center justify-evenly '>
+				<div className='flex items-center justify-evenly'>
 					{responseFields.data.data.length ?
 						<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-6 w-3/4 mt-12'>
 							{responseFields.data.data.map(field => (
 								<div
 									key={field.id}
-									className='bg-white p-6 rounded-lg shadow-md max-w-xs md:max-w-md lg:max-w-lg '
+									className='bg-white p-6 rounded-lg shadow-md max-w-xs md:max-w-md lg:max-w-lg'
 								>
 									<h3 className='text-xl font-semibold mb-2 text-center'>{field.name}</h3>
 									<div className='text-gray-700 mb-2 flex items-center'>
-										<FaMapMarkerAlt className='mr-2 text-red-500 ' />
+										<FaMapMarkerAlt className='mr-2 text-red-500' />
 										<p>{field.location}</p>
 									</div>
 									<div className='text-gray-700 mb-2 flex items-center'>
@@ -108,11 +143,11 @@ const HomePage = () => {
 						</div>
 					:	<div className='text-center text-gray-600'>Lamentamos, ainda não temos nenhum campo disponível.</div>}
 				</div>
-			:	<div className='flex items-center justify-center '>
+			:	<div className='flex items-center justify-center'>
 					<div className='animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900'></div>
 				</div>
 			}
-		</div>
+		</section>
 	);
 };
 
--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
+import backgroundImage from '../../assets/img/campo.jpg';
 
 const SignInPage = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState<string | null>(null);
 	const navigate = useNavigate();
 	const { login, user, isLoading } = useAuth();
@@ -13,78 +15,139 @@ const SignInPage = () => {
 		if (user && !isLoading) {
 			navigate('/');
 		}
-	}, []);
+	}, [user, isLoading, navigate]);
 
 	const handleSignIn = async (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		setError(null);
-
+		setLoading(true);
 		try {
 			await login(email, password);
 			navigate('/');
 		} catch (error) {
 			setError('Credenciais inválidas. Por favor, tente novamente.');
+		} finally {
+			setLoading(false);
 		}
 	};
 
 	return (
-		<div className='container mx-auto p-4 bg-background w-full'>
-			<h1 className='text-3xl font-bold mb-4'>Login</h1>
-			<form onSubmit={handleSignIn}>
-				<div className='mb-4'>
-					<label className='block text-gray-700 mb-2'>Email:</label>
-					<input
-						type='email'
-						name='email'
-						className='w-full p-2 border rounded'
-						required
-						value={email}
-						onChange={e => setEmail(e.target.value)}
-					/>
-				</div>
-				<div className='mb-4'>
-					<label className='block text-gray-700 mb-2'>Senha:</label>
-					<input
-						type='password'
-						name='password'
-						className='w-full p-2 border rounded'
-						required
-						value={password}
-						onChange={e => setPassword(e.target.value)}
-					/>
-				</div>
-				{error && <div className='text-red-500 mb-4'>{error}</div>}
-				<button
-					type='submit'
-					className='w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300'
+		<section
+			className='min-h-screen flex items-center justify-center'
+			style={{
+				backgroundImage: `url(${backgroundImage})`,
+				backgroundSize: 'cover',
+				backgroundPosition: 'center',
+			}}
+		>
+			<div className='bg-white p-8 rounded-lg shadow-md w-full max-w-md md:max-w-xl'>
+				<h1 className='text-4xl font-bold my-3 text-center text-blue-700'>SportReserve</h1>
+				<p className='text-sm mb-6 text-center'>Aluguel fácil, jogo garantido</p>
+
+				<form
+					onSubmit={handleSignIn}
+					className='mx-4 md:mx-auto '
 				>
-					Entrar
-				</button>
-				<div className='flex justify-between mt-4'>
-					<button
-						type='button'
-						className='text-blue-500 hover:underline'
-						onClick={() => navigate('/forgot-password')}
-					>
-						Esqueceu a senha?
-					</button>
-					<button
-						type='button'
-						className='text-blue-500 hover:underline'
-						onClick={() => navigate('/signup')}
-					>
-						Não tem uma conta? Registre-se
-					</button>
-				</div>
-				<button
-					type='button'
-					className='w-full bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-600 transition duration-300 mt-4'
-					onClick={() => navigate('/')}
-				>
-					Voltar
-				</button>
-			</form>
-		</div>
+					<div className='relative mb-6'>
+						<input
+							type='email'
+							name='email'
+							placeholder=' '
+							className='peer block w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500 placeholder-transparent'
+							required
+							value={email}
+							onChange={e => setEmail(e.target.value)}
+						/>
+						<label
+							className={`absolute left-2 top-2 text-gray-500 transition-all duration-300 transform 
+                                peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 
+                                peer-focus:-top-4 peer-focus:text-sm peer-focus:text-gray-500
+                                ${email ? '-top-4 text-sm' : ''}`}
+						>
+							E-mail
+						</label>
+					</div>
+					<div className='relative mb-6'>
+						<input
+							type='password'
+							name='password'
+							placeholder=' '
+							className='peer block w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500 placeholder-transparent'
+							required
+							value={password}
+							onChange={e => setPassword(e.target.value)}
+						/>
+						<label
+							className={`absolute left-2 top-2 text-gray-500 transition-all duration-300 transform 
+                                peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 
+                                peer-focus:-top-4 peer-focus:text-sm peer-focus:text-gray-500
+                                ${password ? '-top-4 text-sm' : ''}`}
+						>
+							Senha
+						</label>
+					</div>
+					{error && <div className='text-red-500 mb-4'>{error}</div>}
+					<div className='flex items-center justify-center'>
+						<button
+							type='submit'
+							className='w-full bg-blue-500 text-white py-3 px-4 rounded-lg hover:bg-blue-600 transition duration-300 relative'
+							disabled={loading}
+						>
+							{loading ?
+								<svg
+									className='animate-spin h-5 w-5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'
+									viewBox='0 0 24 24'
+								>
+									<circle
+										className='opacity-25'
+										cx='12'
+										cy='12'
+										r='10'
+										stroke='currentColor'
+										strokeWidth='4'
+									/>
+									<path
+										className='opacity-75'
+										fill='currentColor'
+										d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zm2-11.732a7.965 7.965 0 014 1.733L9 7.98A5.963 5.963 0 006 12H2c0-2.056.835-3.921 2.196-5.291z'
+									/>
+								</svg>
+							:	'Entrar'}
+						</button>
+					</div>
+					<div className='flex justify-end mt-3 '>
+						<button
+							type='button'
+							className='hover:underline text-sm'
+							onClick={() => navigate('/forgot-password')}
+						>
+							Esqueceu a senha?
+						</button>
+					</div>
+					<div className='flex items-center justify-center'>
+						<button
+							type='button'
+							className='w-full bg-gray-500 text-white py-3 px-4 rounded-lg hover:bg-gray-600 transition duration-300 mt-4'
+							onClick={() => navigate('/')}
+						>
+							Voltar
+						</button>
+					</div>
+
+					<div className='flex justify-center mt-4'>
+						<button
+							type='button'
+							className='text-blue-500 hover:underline'
+							onClick={() => navigate('/signup')}
+						>
+							<p className='text-gray-500'>
+								Não tem uma conta? <span className='text-red-500 font-bold'>Registre-se</span>
+							</p>
+						</button>
+					</div>
+				</form>
+			</div>
+		</section>
 	);
 };
 
