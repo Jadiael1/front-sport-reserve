40e952dec9385c23cf74a0239671ce978392b6a0
Wilksonflor <wilksonflor12@gmail.com>
2024-07-11 16:47:32 -0300

    feat(signup): add form validation and loading state

--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
+import { CiWarning } from 'react-icons/ci';
 
 const RegisterPage = () => {
+	const navigate = useNavigate();
 	const [formData, setFormData] = useState({
 		name: '',
 		email: '',
@@ -11,10 +13,20 @@ const RegisterPage = () => {
 		cpf: '',
 		phone: '',
 	});
+
+	// erros
+	const [errors, setErrors] = useState({
+		name: '',
+		email: '',
+		password: '',
+		password_confirmation: '',
+		cpf: '',
+		phone: '',
+	});
 	const [error, setError] = useState<string | null>(null);
-	const navigate = useNavigate();
 	const { login, user, isLoading } = useAuth();
 	const baseURL = import.meta.env.VITE_API_BASE_URL;
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
 		if (user && !isLoading) {
@@ -27,9 +39,33 @@ const RegisterPage = () => {
 		setFormData({ ...formData, [name]: value });
 	};
 
+	// Validações
+	const isValidEmail = (email: string) => {
+		return /\S+@\S+\.\S+/.test(email);
+	};
+
+	const passwordsMatch = (password: string, confirmPassword: string) => {
+		return password === confirmPassword;
+	};
+
 	const handleRegister = async (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		setError(null);
+		setLoading(true);
+
+		// Validar o email e senha
+		if (!isValidEmail(formData.email)) {
+			setError('Por favor, insira um e-mail válido');
+			setLoading(false);
+			return;
+		}
+
+		if (!passwordsMatch(formData.password, formData.password_confirmation)) {
+			setError('As senhas não conferem');
+			setLoading(false);
+			return;
+		}
+		// 	----
 
 		try {
 			const response = await fetch(`${baseURL}/auth/signup`, {
@@ -41,7 +77,7 @@ const RegisterPage = () => {
 				body: JSON.stringify(formData),
 			});
 			const data = await response.json();
-
+			setLoading(false);
 			if (response.status === 201 && data.status === 'success') {
 				await login(formData.email, formData.password);
 				alert('Registro realizado com sucesso!');
@@ -51,95 +87,148 @@ const RegisterPage = () => {
 			}
 		} catch (error) {
 			setError('Falha ao registrar');
+		} finally {
+			setLoading(false);
 		}
 	};
 
 	return (
-		<div className='container mx-auto p-4'>
-			<h1 className='text-3xl font-bold mb-4'>Registrar</h1>
-			<form onSubmit={handleRegister}>
-				<div className='mb-4'>
-					<label className='block text-gray-700 mb-2'>Nome:</label>
-					<input
-						type='text'
-						name='name'
-						className='w-full p-2 border rounded'
-						required
-						value={formData.name}
-						onChange={handleChange}
-					/>
-				</div>
-				<div className='mb-4'>
-					<label className='block text-gray-700 mb-2'>Email:</label>
-					<input
-						type='email'
-						name='email'
-						className='w-full p-2 border rounded'
-						required
-						value={formData.email}
-						onChange={handleChange}
-					/>
-				</div>
-				<div className='mb-4'>
-					<label className='block text-gray-700 mb-2'>Senha:</label>
-					<input
-						type='password'
-						name='password'
-						className='w-full p-2 border rounded'
-						required
-						value={formData.password}
-						onChange={handleChange}
-					/>
-				</div>
-				<div className='mb-4'>
-					<label className='block text-gray-700 mb-2'>Confirme a Senha:</label>
-					<input
-						type='password'
-						name='password_confirmation'
-						className='w-full p-2 border rounded'
-						required
-						value={formData.password_confirmation}
-						onChange={handleChange}
-					/>
-				</div>
-				<div className='mb-4'>
-					<label className='block text-gray-700 mb-2'>CPF:</label>
-					<input
-						type='text'
-						name='cpf'
-						className='w-full p-2 border rounded'
-						required
-						value={formData.cpf}
-						onChange={handleChange}
-					/>
-				</div>
-				<div className='mb-4'>
-					<label className='block text-gray-700 mb-2'>Telefone:</label>
-					<input
-						type='text'
-						name='phone'
-						className='w-full p-2 border rounded'
-						required
-						value={formData.phone}
-						onChange={handleChange}
-					/>
-				</div>
-				{error && <div className='text-red-500 mb-4'>{error}</div>}
-				<button
-					type='submit'
-					className='w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300'
+		<section className='flex items-center justify-center min-h-screen bg-slate-200'>
+			<div className='container mx-auto p-6 max-w-lg'>
+				<h1 className='text-3xl font-bold mb-6 text-center'>SportReserve</h1>
+				<p className='text-lg text-gray-700 mb-8 text-center'>Crie sua conta para começar a reservar!</p>
+				<form
+					onSubmit={handleRegister}
+					className='bg-white p-8 rounded-lg shadow-lg'
 				>
-					Registrar
-				</button>
-				<button
-					type='button'
-					className='w-full bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-600 transition duration-300 mt-4'
-					onClick={() => navigate('/signin')}
-				>
-					Voltar
-				</button>
-			</form>
-		</div>
+					<div className='relative mb-6'>
+						<input
+							type='text'
+							name='name'
+							placeholder=' '
+							className='block px-2.5 pb-2.5 pt-3.5 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+							required
+							value={formData.name}
+							onChange={handleChange}
+						/>
+						<label className='absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-3 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-100 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-3 peer-focus:bg-white peer-placeholder-shown:bg-transparent'>
+							Nome
+						</label>
+						{errors.name && <div className='text-red-500 text-sm mt-1'>{errors.name}</div>}
+					</div>
+					<div className='relative mb-6'>
+						<input
+							type='email'
+							name='email'
+							placeholder=' '
+							className='block px-2.5 pb-2.5 pt-3.5 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+							required
+							value={formData.email}
+							onChange={handleChange}
+						/>
+						<label className='absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-3 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-100 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-3 peer-focus:bg-white peer-placeholder-shown:bg-transparent'>
+							Email
+						</label>
+						{errors.email && (
+							<div className='text-red-500 text-sm mt-1'>
+								<CiWarning /> {errors.email}
+							</div>
+						)}
+					</div>
+					<div className='relative mb-6'>
+						<input
+							type='password'
+							name='password'
+							placeholder=' '
+							className='block px-2.5 pb-2.5 pt-3.5 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+							required
+							value={formData.password}
+							onChange={handleChange}
+						/>
+						<label className='absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-3 z-10 origin-[100] bg-white px-3 peer-focus:px-1 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-100 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-3 peer-focus:bg-white peer-placeholder-shown:bg-transparent'>
+							Senha
+						</label>
+						{errors.password && (
+							<div className='text-red-500 text-sm mt-1'>
+								<CiWarning /> {errors.password}
+							</div>
+						)}
+					</div>
+					<div className='relative mb-6'>
+						<input
+							type='password'
+							name='password_confirmation'
+							placeholder=' '
+							className='block px-2.5 pb-2.5 pt-3.5 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+							required
+							value={formData.password_confirmation}
+							onChange={handleChange}
+						/>
+						<label className='absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-3 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-100 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-3 peer-focus:bg-white peer-placeholder-shown:bg-transparent'>
+							Confirme a Senha
+						</label>
+						{errors.password_confirmation && (
+							<div className='text-red-500 text-sm mt-1'>
+								<CiWarning /> {errors.password_confirmation}
+							</div>
+						)}
+					</div>
+					<div className='relative mb-6'>
+						<input
+							type='text'
+							name='cpf'
+							placeholder=' '
+							className='block px-2.5 pb-2.5 pt-3.5 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+							required
+							value={formData.cpf}
+							onChange={handleChange}
+						/>
+						<label className='absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-3 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-100 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-3 peer-focus:bg-white peer-placeholder-shown:bg-transparent'>
+							CPF
+						</label>
+						{errors.cpf && (
+							<div className='text-red-500 text-sm mt-1'>
+								<CiWarning /> {errors.cpf}
+							</div>
+						)}
+					</div>
+					<div className='relative mb-6'>
+						<input
+							type='text'
+							name='phone'
+							placeholder=' '
+							className='block px-2.5 pb-2.5 pt-3.5 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+							required
+							value={formData.phone}
+							onChange={handleChange}
+						/>
+						<label className='absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-3 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-100 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-3 peer-focus:bg-white peer-placeholder-shown:bg-transparent'>
+							Telefone
+						</label>
+						{errors.phone && (
+							<div className='text-red-500 text-sm mt-1'>
+								<CiWarning /> {errors.phone}
+							</div>
+						)}
+					</div>
+					{error && <p className='text-red-500 text-sm mt-2 text-center mb-3'>{error}</p>}
+					<button
+						type='submit'
+						disabled={loading}
+						className='w-full px-3 py-3 rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:bg-blue-700'
+					>
+						{loading ? 'Registrando...' : 'Registrar'}
+					</button>
+					<button
+						type='button'
+						className='w-full bg-gray-500 text-white py-2 px-4 rounded-lg hover:bg-gray-600 transition duration-300 mt-4 font-semibold'
+						onClick={() => navigate('/signin')}
+					>
+						Voltar
+					</button>
+				</form>
+			</div>
+		</section>
 	);
 };
 
