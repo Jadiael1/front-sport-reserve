cbc722b1276868932ac1d0b8c08fc68bec55aed1
Wilksonflor <wilksonflor12@gmail.com>
2024-07-11 16:47:22 -0300

    style(SignIn): Update input and button styles

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -53,36 +53,27 @@ const SignInPage = () => {
 							type='email'
 							name='email'
 							placeholder=' '
-							className='peer block w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500 placeholder-transparent'
+							className='block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
 							required
 							value={email}
 							onChange={e => setEmail(e.target.value)}
 						/>
-						<label
-							className={`absolute left-2 top-2 text-gray-500 transition-all duration-300 transform 
-                                peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 
-                                peer-focus:-top-4 peer-focus:text-sm peer-focus:text-gray-500
-                                ${email ? '-top-4 text-sm' : ''}`}
-						>
+						<label className='absolute text-sm text-dark-500 bg-white dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1'>
 							E-mail
 						</label>
 					</div>
+
 					<div className='relative mb-6'>
 						<input
 							type='password'
 							name='password'
 							placeholder=' '
-							className='peer block w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500 placeholder-transparent'
+							className='block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
 							required
 							value={password}
 							onChange={e => setPassword(e.target.value)}
 						/>
-						<label
-							className={`absolute left-2 top-2 text-gray-500 transition-all duration-300 transform 
-                                peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 
-                                peer-focus:-top-4 peer-focus:text-sm peer-focus:text-gray-500
-                                ${password ? '-top-4 text-sm' : ''}`}
-						>
+						<label className='absolute text-sm text-dark-500 bg-white dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1'>
 							Senha
 						</label>
 					</div>
@@ -94,24 +85,26 @@ const SignInPage = () => {
 							disabled={loading}
 						>
 							{loading ?
-								<svg
-									className='animate-spin h-5 w-5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'
-									viewBox='0 0 24 24'
-								>
-									<circle
-										className='opacity-25'
-										cx='12'
-										cy='12'
-										r='10'
-										stroke='currentColor'
-										strokeWidth='4'
-									/>
-									<path
-										className='opacity-75'
-										fill='currentColor'
-										d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zm2-11.732a7.965 7.965 0 014 1.733L9 7.98A5.963 5.963 0 006 12H2c0-2.056.835-3.921 2.196-5.291z'
-									/>
-								</svg>
+								<>
+									<div className='flex items-center justify-center'>
+										<svg
+											aria-hidden='true'
+											className=' w-8 h-8 text-white animate-spin dark:text-white fill-blue-600'
+											viewBox='0 0 100 101'
+											fill='none'
+											xmlns='http://www.w3.org/2000/svg'
+										>
+											<path
+												d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
+												fill='currentColor'
+											/>
+											<path
+												d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
+												fill='currentFill'
+											/>
+										</svg>
+									</div>
+								</>
 							:	'Entrar'}
 						</button>
 					</div>
