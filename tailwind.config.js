13565bf1720d033d6d03c7ee4b6cd23f264b6276
Jadiael1 <jadiael@hotmail.com.br>
2024-07-16 09:11:52 -0300

    refactor(tailwind): simplify content paths in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
 	content: [
 		'./src/**/*.{ts,tsx}', // Procura por classes Tailwind em todos os arquivos JS, JSX, TS, e TSX dentro da pasta src
-		'./src/components/atoms/**/*.{ts,tsx}', // Procura especificamente dentro da pasta atoms
-		'./src/components/molecules/**/*.{ts,tsx}', // Procura especificamente dentro da pasta molecules
-		'./src/components/organisms/**/*.{ts,tsx}', // Procura especificamente dentro da pasta organisms
-		'./src/components/templates/**/*.{ts,tsx}', // Procura especificamente dentro da pasta templates
-		'./src/components/pages/**/*.{ts,tsx}', // Procura especificamente dentro da pasta pages
+		'./src/components/**/*.{ts,tsx}' // Procura especificamente dentro da pasta components
 	],
 	theme: {
 		extend: {
