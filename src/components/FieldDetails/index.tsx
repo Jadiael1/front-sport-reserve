afd9b011557c2f28ddce7cdd96604fa06397f488
Wilksonflor <wilksonflor12@gmail.com>
2024-07-21 10:28:09 -0300

    refactor(FieldDetails): update state types and validation

--- src/components/FieldDetails/index.tsx
+++ src/components/FieldDetails/index.tsx
@@ -4,20 +4,26 @@ import { useAuth } from '../../contexts/AuthContext';
 import Alert from '../Alert';
 import { DatePicker } from 'antd';
 import { IField } from '../Home/interfaces/IFields';
+import moment from 'moment';
 
 interface FieldDetailsProps {
 	field: IField;
 }
+
 const FieldDetails = ({ field }: FieldDetailsProps) => {
 	const { token } = useAuth();
-	const [startTime, setStartTime] = useState('');
-	const [endTime, setEndTime] = useState('');
+	const [startTime, setStartTime] = useState<moment.Moment | null>(null);
+	const [endTime, setEndTime] = useState<moment.Moment | null>(null);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState<{ message: string; errors?: { [key: string]: string[] } } | null>(null);
 	const [success, setSuccess] = useState<{ message: string } | null>(null);
 	const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 	const handleReservation = () => {
+		if (!startTime || !endTime) {
+			alert('Por favor, selecione a data e hora de início e término.');
+			return;
+		}
 		setLoading(true);
 		fetch(`${baseURL}/reservations`, {
 			method: 'POST',
@@ -28,20 +34,21 @@ const FieldDetails = ({ field }: FieldDetailsProps) => {
 			},
 			body: JSON.stringify({
 				field_id: field.id,
-				start_time: startTime,
-				end_time: endTime,
+				start_time: startTime.toISOString(),
+				end_time: endTime.toISOString(),
 			}),
 		})
 			.then(resp => resp.json())
 			.then(data => {
+				setLoading(false);
 				if (data.status === 'success') {
 					setSuccess({ message: data.message });
 				} else {
-					setLoading(false);
 					setError({ message: data.message, errors: data.errors });
 				}
 			})
 			.catch(error => {
+				setLoading(false);
 				console.error('Error:', error);
 				alert('Erro ao realizar a reserva. Tente novamente.');
 			});
@@ -52,7 +59,7 @@ const FieldDetails = ({ field }: FieldDetailsProps) => {
 			{success && (
 				<Alert
 					message={success.message}
-					onClose={() => setError(null)}
+					onClose={() => setSuccess(null)}
 					type='success'
 					redirectTo='/reservations'
 				/>
@@ -94,12 +101,6 @@ const FieldDetails = ({ field }: FieldDetailsProps) => {
 							<label className='absolute text-sm text-dark-500 bg-white dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1'>
 								Hora de Início
 							</label>
-							{/* <input
-								type='datetime-local'
-								className='block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-								value={startTime}
-								onChange={e => setStartTime(e.target.value)}
-							/> */}
 							<DatePicker
 								showTime={{ format: 'HH:mm' }}
 								format='DD/MM/YYYY HH:mm'
@@ -113,12 +114,6 @@ const FieldDetails = ({ field }: FieldDetailsProps) => {
 							<label className='absolute text-sm text-dark-500 bg-white dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1'>
 								Hora de Término
 							</label>
-							{/* <input
-								type='datetime-local'
-								className='block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-								value={endTime}
-								onChange={e => setEndTime(e.target.value)}
-							/> */}
 							<DatePicker
 								showTime={{ format: 'HH:mm' }}
 								format='DD/MM/YYYY HH:mm'
@@ -138,7 +133,6 @@ const FieldDetails = ({ field }: FieldDetailsProps) => {
 							disabled={loading}
 						>
 							{loading ? 'Reservando...' : 'Reservar'}
-							{/* // Confirmar Reserva */}
 						</button>
 					</div>
 				</div>
