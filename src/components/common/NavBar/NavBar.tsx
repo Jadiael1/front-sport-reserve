b3076585497ec4e2c755228a94314ebc09f6907d
Jadiael1 <jadiael@hotmail.com.br>
2024-07-24 13:30:23 -0300

    refactor(NavBar): update component structure and style

--- src/components/common/NavBar/NavBar.tsx
+++ src/components/common/NavBar/NavBar.tsx
@@ -1,16 +1,16 @@
-// Navbar.jsx
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../../hooks/useAuth';
-import { IoMdMenu, IoIosLogOut } from 'react-icons/io';
+import { IoMdMenu, IoIosLogOut, IoMdFootball } from 'react-icons/io';
+import { CiLogin } from 'react-icons/ci';
 import { useState } from 'react';
 
-export const Navbar = () => {
+const Navbar = () => {
 	const { user, logout } = useAuth();
 	const navigate = useNavigate();
 	const [menuOpen, setMenuOpen] = useState(false);
 
 	const handleGoReservationList = () => {
-		navigate(`/reservations`);
+		navigate('/reservations');
 		if (menuOpen) setMenuOpen(false);
 	};
 
@@ -19,73 +19,93 @@ export const Navbar = () => {
 		if (menuOpen) setMenuOpen(false);
 	};
 
+	const handleGoSignIn = () => {
+		navigate('/signin');
+	};
+
+	const handleGoHome = () => {
+		navigate('/');
+	};
+
 	return (
-		<nav className='flex items-center gap-5'>
-			<button
-				className='block md:hidden bg-gray-200 px-3 py-2 rounded bg-transparent'
-				onClick={() => setMenuOpen(!menuOpen)}
-			>
-				<IoMdMenu className='text-3xl' />
-			</button>
-
-			{menuOpen && (
-				<div className='absolute right-0 top-14 mt-6 bg-white w-full md:max-w-md lg:max-w-lg shadow-md rounded-md py-2 z-50'>
-					{user?.is_admin && (
-						<button
-							className='mt-1 px-4 py-2 text-gray-800 hover:bg-gray-200 w-full bg-white font-bold'
-							onClick={() => {
-								navigate('/fields/new');
-								setMenuOpen(false);
-							}}
-						>
-							Criar novo campo
-						</button>
-					)}
-
-					<button
-						className='px-2 py-2 text-gray-800 hover:bg-gray-200 w-full gap-3 font-bold'
-						onClick={handleGoReservationList}
+		<nav className='bg-background border-b border-gray-200'>
+			<div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+				<div
+					className='flex items-center space-x-3 rtl:space-x-reverse cursor-pointer'
+					onClick={handleGoHome}
+				>
+					<IoMdFootball className='h-8 w-8 text-blue-700' />
+					<span
+						className='self-center text-2xl font-semibold whitespace-nowrap'
+						onClick={handleGoHome}
 					>
-						Ver minhas reservas
-					</button>
-
-					<button
-						className='mt-1 px-4 py-2 text-gray-800 hover:bg-gray-200 w-full  gap-3 bg-white font-bold'
-						onClick={handleLogout}
-					>
-						{/* <IoIosLogOut className='text-base' /> */}
-						Sair
-					</button>
+						SportReserve
+					</span>
 				</div>
-			)}
-
-			<div className='hidden md:flex items-center gap-5'>
-				{user?.is_admin && (
-					<button
-						className='font-bold hover:opacity-60'
-						onClick={() => {
-							navigate('/fields/new');
-							setMenuOpen(false);
-						}}
-					>
-						Criar novo campo
-					</button>
-				)}
 				<button
-					className='text-dark py-2 px-4 font-bold rounded transition duration-300 flex items-center gap-3 hover:opacity-60'
-					onClick={handleGoReservationList}
+					data-collapse-toggle='navbar-default'
+					type='button'
+					className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+					aria-controls='navbar-default'
+					aria-expanded={menuOpen ? 'true' : 'false'}
+					onClick={() => setMenuOpen(!menuOpen)}
 				>
-					Ver minhas reservas
+					<span className='sr-only'>Open main menu</span>
+					<IoMdMenu className='w-5 h-5' />
 				</button>
-
-				<button
-					className='bg-black text-white py-2 px-4 font-bold  rounded border border-transparent hover:border-black hover:text-black hover:bg-transparent transition duration-300 flex items-center gap-3'
-					onClick={handleLogout}
+				<div
+					className={`${menuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`}
+					id='navbar-default'
 				>
-					<IoIosLogOut className='text-base' />
-					Sair
-				</button>
+					<ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:border-gray-700'>
+						{user ?
+							<>
+								{user.is_admin && (
+									<li>
+										<button
+											className='block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 transition duration-300'
+											onClick={() => {
+												navigate('/fields/new');
+												setMenuOpen(false);
+											}}
+										>
+											Criar novo campo
+										</button>
+									</li>
+								)}
+								<li>
+									<button
+										className='block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 transition duration-300'
+										onClick={handleGoReservationList}
+									>
+										Ver minhas reservas
+									</button>
+								</li>
+								<li>
+									<button
+										className='flex items-center py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 transition duration-300'
+										onClick={handleLogout}
+									>
+										<IoIosLogOut className='text-base mr-1' />
+										Sair
+									</button>
+								</li>
+							</>
+						:	<li>
+								<button
+									className='flex items-center justify-center gap-1 w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-500 transition duration-300'
+									onClick={handleGoSignIn}
+								>
+									<CiLogin className='text-2xl' />
+									Entrar
+								</button>
+							</li>
+						}
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
 };
+
+export default Navbar;
