0b687b57eacd6ff6b75fdb3de169d1421c798ac1
Jadiael1 <jadiael@hotmail.com.br>
2024-07-24 14:03:17 -0300

    feat(auth): enhance email verification UI and add timer

--- src/pages/auth/EmailVerification/index.tsx
+++ src/pages/auth/EmailVerification/index.tsx
@@ -1,15 +1,30 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSearchParams, useNavigate } from 'react-router-dom';
-import { useAuth } from '../../../hooks/useAuth.js';
+import { useAuth } from '../../../hooks/useAuth';
+import { FaCheckCircle, FaTimesCircle, FaHourglassHalf } from 'react-icons/fa';
 
-const EmailVerification = () => {
+const EmailVerification: React.FC = () => {
+	const timeRedirect = 4;
 	const [searchParams] = useSearchParams();
 	const [message, setMessage] = useState<string | null>(null);
 	const [error, setError] = useState<string | null>(null);
+	const [timeLeft, setTimeLeft] = useState<number>(timeRedirect);
 	const navigate = useNavigate();
 	const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 	const { user } = useAuth();
+
+	useEffect(() => {
+		const timerId = setInterval(() => {
+			setTimeLeft(prevTime => prevTime - 1);
+		}, 1000);
+
+		if (timeLeft <= 0) {
+			clearInterval(timerId);
+			return;
+		}
+		return () => clearInterval(timerId);
+	}, [timeLeft]);
 
 	useEffect(() => {
 		const id = searchParams.get('id');
@@ -27,7 +42,7 @@ const EmailVerification = () => {
 	const verifyEmail = async (id: string, expires: string, signature: string) => {
 		if (user?.email_verified_at) {
 			setMessage('Email verificado com sucesso! Você será redirecionado em breve.');
-			setTimeout(() => navigate('/'), 3000);
+			setTimeout(() => navigate('/'), timeRedirect * 1000);
 		} else {
 			try {
 				const response = await fetch(`${baseURL}/auth/email/verify/${id}?expires=${expires}&signature=${signature}`, {
@@ -42,7 +57,7 @@ const EmailVerification = () => {
 				if (response.status === 200 && data.status === 'success') {
 					setMessage('Email verificado com sucesso! Você será redirecionado em breve.');
 					// fazendo location para que a aplicação faça um novo loadUser e atualize o campo email_verified_at do usuario.
-					setTimeout(() => window.location.replace('/'), 3000);
+					setTimeout(() => window.location.replace('/'), timeRedirect * 1000);
 				} else {
 					setError(data.message || 'Falha ao verificar o email.');
 				}
@@ -53,10 +68,30 @@ const EmailVerification = () => {
 	};
 
 	return (
-		<div className='container mx-auto p-4'>
-			<h1 className='text-3xl font-bold mb-4'>Verificação de Email</h1>
-			{message && <div className='text-green-500'>{message}</div>}
-			{error && <div className='text-red-500'>{error}</div>}
+		<div className='flex items-center justify-center min-h-screen bg-gray-100'>
+			<div className='bg-white p-6 rounded-lg shadow-lg text-center max-w-md w-full'>
+				<h1 className='text-3xl font-bold mb-4'>Verificação de Email</h1>
+				{message && (
+					<div className='flex items-center justify-center space-x-2 text-green-500 mb-4 animate-pulse'>
+						<FaCheckCircle size={24} />
+						<span>
+							{message} {timeLeft} s
+						</span>
+					</div>
+				)}
+				{error && (
+					<div className='flex items-center justify-center space-x-2 text-red-500 mb-4'>
+						<FaTimesCircle size={24} />
+						<span>{error}</span>
+					</div>
+				)}
+				{!message && !error && (
+					<div className='flex items-center justify-center space-x-2 text-yellow-500 mb-4'>
+						<FaHourglassHalf size={24} />
+						<span>Verificando...</span>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 };
