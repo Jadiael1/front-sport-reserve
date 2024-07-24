699209f588715d1c2b415949afd25378fb967a8b
Jadiael1 <jadiael@hotmail.com.br>
2024-07-24 18:17:09 -0300

    feat: Add AnimateSpin component and loading states

--- src/pages/FieldUpdate/index.tsx
+++ src/pages/FieldUpdate/index.tsx
@@ -7,6 +7,7 @@ import { messageManager } from '../../components/common/Message/messageInstance'
 import { FaArrowLeft } from 'react-icons/fa';
 import { LuImagePlus } from 'react-icons/lu';
 import ConfirmationModal from '../../components/common/ConfirmationModalProps';
+import AnimateSpin from '../../assets/svg/AnimateSpin';
 
 const FieldUpdateForm = () => {
 	const location = useLocation();
@@ -37,6 +38,7 @@ const FieldUpdateForm = () => {
 		formData.append('hourly_rate', hourlyRate.toString());
 
 		setLoading(true);
+		await new Promise(resolve => setTimeout(resolve, 10000));
 		try {
 			const response = await fetch(`${baseURL}/fields/${field.id}`, {
 				method: 'POST',
@@ -184,61 +186,85 @@ const FieldUpdateForm = () => {
 	return (
 		<section className='w-full bg-gray-100 min-h-screen flex justify-center items-center'>
 			<div className='w-full max-w-2xl mx-auto bg-white p-6 rounded-lg shadow-lg'>
+				<button
+					className='flex items-center px-4 py-2 text-black rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-300 ease-in-out transform hover:scale-105'
+					onClick={() => navigate(-1)}
+				>
+					<FaArrowLeft className='mr-2' />
+					Voltar
+				</button>
 				<h1 className='text-3xl font-bold mb-4 text-center'>Atualizar arena</h1>
-				{/* {message && <div className='text-green-500 mb-4'>{message}</div>} */}
 				{error && <div className='text-red-500 mb-4'>{error}</div>}
-				<div className='mb-4 relative'>
+
+				<div className='mb-4'>
 					<label
 						htmlFor='name'
-						className='absolute text-sm text-dark-500 bg-white dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1'
+						className='block text-sm font-medium text-gray-700'
 					>
 						Nome da arena
 					</label>
 					<input
 						type='text'
-						className='block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+						id='name'
+						className={`block w-full px-3 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 ${loading ? 'bg-gray-400 text-gray-700 cursor-not-allowed' : 'bg-white text-gray-900'}`}
 						value={name}
+						disabled={loading}
 						onChange={e => setName(e.target.value)}
 					/>
 				</div>
-				<div className='mb-4 relative'>
+
+				<div className='mb-4'>
 					<label
 						htmlFor='location'
-						className='absolute text-sm text-dark-500 bg-white dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1'
+						className='block text-sm font-medium text-gray-700'
 					>
 						Localização
 					</label>
 					<input
 						type='text'
-						className='block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+						id='location'
+						className={`block w-full px-3 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 ${loading ? 'bg-gray-400 text-gray-700 cursor-not-allowed' : 'bg-white text-gray-900'}`}
 						value={locationField}
+						disabled={loading}
 						onChange={e => setLocationField(e.target.value)}
 					/>
 				</div>
-				<div className='mb-4 relative'>
-					<label className='absolute text-sm text-dark-500 bg-white dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1'>
+
+				<div className='mb-4'>
+					<label
+						htmlFor='type'
+						className='block text-sm font-medium text-gray-700'
+					>
 						Modalidade
 					</label>
 					<input
 						type='text'
-						className='block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+						id='type'
+						className={`block w-full px-3 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 ${loading ? 'bg-gray-400 text-gray-700 cursor-not-allowed' : 'bg-white text-gray-900'}`}
 						value={type}
+						disabled={loading}
 						onChange={e => setType(e.target.value)}
 					/>
 				</div>
-				<div className='mb-4 relative'>
-					<label className='absolute text-sm text-dark-500 bg-white dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1'>
+
+				<div className='mb-4'>
+					<label
+						htmlFor='hourlyRate'
+						className='block text-sm font-medium text-gray-700'
+					>
 						Valor por hora
 					</label>
 					<input
 						type='number'
-						className='block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+						id='hourlyRate'
+						className={`block w-full px-3 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 ${loading ? 'bg-gray-400 text-gray-700 cursor-not-allowed' : 'bg-white text-gray-900'}`}
 						value={hourlyRate}
+						disabled={loading}
 						onChange={e => setHourlyRate(parseFloat(e.target.value))}
 					/>
 				</div>
 
-				<h3 className='text-xl font-semibold mb-2 text-center my-2'>Imagens da arena</h3>
+				<h3 className='text-xl font-semibold mb-2 text-center'>Imagens da arena</h3>
 
 				<div className='flex items-center justify-between p-3 flex-wrap'>
 					<p className='font-bold mb-2'>
@@ -247,34 +273,37 @@ const FieldUpdateForm = () => {
 
 					<input
 						type='file'
-						id={`file-input-new`}
+						id='file-input-new'
 						className='hidden'
+						disabled={loading}
 						onChange={evt => handleStoreImage(evt.target.files)}
 					/>
 					<label
-						htmlFor={`file-input-new`}
-						className='bg-blue-500 text-white py-2 px-4 rounded cursor-pointer hover:bg-blue-600 transition duration-300 flex items-center gap-1'
+						htmlFor='file-input-new'
+						className={`py-2 px-4 rounded flex items-center gap-1 ${loading ? 'bg-gray-400 text-gray-700 cursor-not-allowed' : 'bg-blue-500 text-white cursor-pointer hover:bg-blue-600'} transition duration-300`}
 					>
 						<LuImagePlus />
 						Adicionar imagem
 					</label>
 				</div>
+
 				{images.length === 0 ?
-					<p className='text-gray-500'>Nenhuma imagem dispónivel</p>
-				:	<div className='flex flex-wrap gap-4 scroll-auto '>
+					<p className='text-gray-500'>Nenhuma imagem disponível</p>
+				:	<div className='flex flex-wrap gap-4'>
 						{images.map(image => (
 							<div
 								key={image.id}
-								className='relative flex flex-col items-center bg-gray-100  p-3 rounded-xl'
+								className='relative flex flex-col items-center bg-gray-100 p-3 rounded-xl'
 							>
 								<img
 									src={`${baseURL}/${image.path}`.replace('api/v1/', 'public')}
 									alt={`Field ${field.name}`}
-									className='w-full h-32 object-cover mb-2 rounded '
+									className='w-full h-32 object-cover mb-2 rounded'
 								/>
 								<button
-									onClick={() => setIsModalOpen(true)}
-									className='absolute top-[-5px] right-0 text-red-500 bg-white border border-red-500 p-1 rounded-full hover:text-white hover:bg-red-500 transition duration-300'
+									onClick={() => !loading && setIsModalOpen(true)}
+									className={`absolute top-[-5px] right-0 p-1 rounded-full transition duration-300 ${loading ? 'bg-gray-400 text-gray-700 cursor-not-allowed' : 'text-red-500 bg-white hover:text-white hover:bg-red-500 border border-red-500'}`}
+									disabled={loading}
 								>
 									<FaTrash size={16} />
 								</button>
@@ -292,29 +321,15 @@ const FieldUpdateForm = () => {
 									id={`file-input-${image.id}`}
 									className='hidden'
 									onChange={handleImageChange(image.id)}
+									disabled={loading}
 								/>
 								<label
 									htmlFor={`file-input-${image.id}`}
-									className='bg-blue-500 text-white py-2 px-4 rounded cursor-pointer hover:bg-blue-600 transition duration-300'
+									className={`py-2 px-4 rounded transition duration-300 ${loading ? 'bg-gray-400 text-gray-700 cursor-not-allowed' : 'bg-blue-500 text-white hover:bg-blue-600 cursor-pointer'}`}
 								>
 									{loading ?
 										<div className='flex items-center justify-center'>
-											<svg
-												aria-hidden='true'
-												className='w-8 h-8 text-white animate-spin dark:text-white fill-blue-600'
-												viewBox='0 0 100 101'
-												fill='none'
-												xmlns='http://www.w3.org/2000/svg'
-											>
-												<path
-													d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
-													fill='currentColor'
-												/>
-												<path
-													d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
-													fill='currentFill'
-												/>
-											</svg>
+											<AnimateSpin />
 										</div>
 									:	'Atualizar imagem'}
 								</label>
@@ -322,41 +337,18 @@ const FieldUpdateForm = () => {
 						))}
 					</div>
 				}
-				<div className='flex flex-col items-center '>
+
+				<div className='flex justify-center mt-8'>
 					<button
-						className='mt-8 w-2/4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300'
+						className={`w-1/2 py-2 px-4 rounded transition duration-300 ${loading ? 'bg-gray-400 text-gray-700 cursor-not-allowed' : 'bg-blue-500 text-white hover:bg-blue-600'}`}
 						onClick={handleUpdateField}
 						disabled={loading}
 					>
 						{loading ?
-							<>
-								<div className='flex items-center justify-center'>
-									<svg
-										aria-hidden='true'
-										className=' w-8 h-8 text-white animate-spin dark:text-white fill-blue-600'
-										viewBox='0 0 100 101'
-										fill='none'
-										xmlns='http://www.w3.org/2000/svg'
-									>
-										<path
-											d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
-											fill='currentColor'
-										/>
-										<path
-											d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
-											fill='currentFill'
-										/>
-									</svg>
-								</div>
-							</>
+							<div className='flex items-center justify-center'>
+								<AnimateSpin />
+							</div>
 						:	'Atualizar arena'}
-					</button>
-					<button
-						onClick={() => navigate('/')}
-						className='flex items-center justify-center gap-1 w-1/2 my-3 bg-slate-300 py-2 px-4'
-					>
-						<FaArrowLeft />
-						Voltar
 					</button>
 				</div>
 			</div>
