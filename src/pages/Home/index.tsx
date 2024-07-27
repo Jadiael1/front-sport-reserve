c1a8a6703cb86a5804c0d25003c7318f0a27c0b3
Jadiael1 <jadiael@hotmail.com.br>
2024-07-27 13:14:33 -0300

    feat: Add field deletion functionality with modal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,25 +6,25 @@ import { useAuth } from '../../hooks/useAuth';
 import Modal from '../../components/common/Modal';
 import Carousel from '../../components/common/Carousel';
 import Navbar from '../../components/common/NavBar/NavBar';
-import { FaFutbol, FaMapMarkerAlt, FaDollarSign } from 'react-icons/fa';
+import { FaFutbol, FaMapMarkerAlt, FaDollarSign, FaShoppingCart, FaEdit, FaTrash } from 'react-icons/fa';
 import FieldDetails from '../FieldDetails';
 import { Gallery, Item } from 'react-photoswipe-gallery';
 import 'photoswipe/dist/photoswipe.css';
+import ConfirmationModal from '../../components/common/ConfirmationModalProps';
 
 const HomePage = () => {
-	const { user } = useAuth();
+	const { user, token } = useAuth();
 	const [responseFields, setResponseFields] = useState<IApiFieldResponse | null>(null);
 	const [selectedField, setSelectedField] = useState<IField | null>(null);
 	const [isFieldDetailsModalVisible, setIsFieldDetailsModalVisible] = useState<boolean>(false);
 	const [intrinsicSize, setIntrinsicSize] = useState<{ [key: string]: { width: number; height: number } }>({});
+	const [confirmationModal, setConfirmationModal] = useState<boolean>(false);
+	const [fieldToDelete, setFieldToDelete] = useState<IField | null>(null);
 	const baseURL = import.meta.env.VITE_API_BASE_URL;
 	const navigate = useNavigate();
 	const galleryID = 'my-gallery';
 
-	// const arrowSVGString = '<svg aria-hidden="true" class="pswp__icn" viewBox="0 0 100 125" width="100" height="125"><path d="M5,50L50,5l3,3L11,50l42,42l-3,3L5,50z M92,95l3-3L53,50L95,8l-3-3L47,50L92,95z"/></svg>';
 	const photoswipeOptions = {
-		// arrowPrevSVG: arrowSVGString,
-		// arrowNextSVG: arrowSVGString,
 		closeTitle: 'Fechar a caixa de diálogo',
 		zoomTitle: 'Amplie a foto',
 		arrowPrevTitle: 'Ir para a foto anterior',
@@ -32,6 +32,7 @@ const HomePage = () => {
 		errorMsg: 'A foto não pode ser carregada',
 		indexIndicatorSep: ' de ',
 	};
+
 	const handleImageLoad = (event: React.SyntheticEvent<HTMLImageElement>, imagePath: string) => {
 		const { naturalWidth, naturalHeight } = event.currentTarget;
 		setIntrinsicSize(prevSizes => ({
@@ -65,49 +66,83 @@ const HomePage = () => {
 		navigate(`/fields/edit/${field.id}`, { state: { field } });
 	};
 
+	const handleDeleteClick = async (field: IField) => {
+		setFieldToDelete(field);
+		// open confirmation modal
+		setConfirmationModal(true);
+	};
+
+	const confirmDeleteField = async () => {
+		if (!fieldToDelete) return;
+		try {
+			const response = await fetch(`${baseURL}/fields/${fieldToDelete.id}`, {
+				method: 'DELETE',
+				headers: {
+					Accept: 'application/json',
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			const result = await response.json();
+			if (result.status === 'success') {
+				setResponseFields(prevFields => {
+					if (!prevFields) return prevFields;
+					return {
+						...prevFields,
+						data: {
+							...prevFields.data,
+							data: prevFields.data.data.filter(field => field.id !== fieldToDelete.id),
+						},
+					};
+				});
+				alert('Quadra deletada com sucesso.');
+			} else {
+				alert('Falha ao deletar a quadra.');
+			}
+		} catch (error) {
+			console.error('Error deleting field:', error);
+			alert('Erro ao deletar a quadra.');
+		} finally {
+			setFieldToDelete(null);
+			setConfirmationModal(false);
+		}
+	};
+
 	return (
 		<>
 			<Navbar />
-			<section className='mx-auto p-2 sm:p-3 md:p-4 lg:p- w-full bg-background min-h-screen'>
+			<section className='mx-auto p-4 w-full bg-background min-h-screen'>
 				<div className='text-center mb-8'>
-					<h3 className='text-dark text-2xl sm:text-2xl md:text-3xl font-bold mt-5'>
+					<h3 className='text-dark text-2xl md:text-3xl font-bold mt-5'>
 						Alugue nossas quadras esportivas com facilidade.
 					</h3>
-					<p className='mt-5'>Escolha uma das nossas quadras </p>
+					<p className='mt-2'>Escolha uma das nossas quadras</p>
 				</div>
 
-				{/* Conteúdo dos campos */}
 				{responseFields ?
-					<div className='flex items-center justify-evenly'>
+					<div className='flex flex-wrap justify-evenly'>
 						{responseFields.data.data.length ?
-							<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-6 w-3/4 mt-12'>
+							<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-6 w-full'>
 								{responseFields.data.data.map(field => (
 									<div
 										key={field.id}
-										className='bg-white p-6 rounded-lg shadow-md max-w-xs md:max-w-md lg:max-w-lg'
+										className='bg-white p-6 rounded-lg shadow-md w-full max-w-sm mx-auto'
 									>
 										<h3 className='text-xl font-semibold mb-2 text-center'>{field.name}</h3>
 
-										<div className='text-gray-700 mb-2 flex items-center mt-3'>
-											<div className='flex items-center gap-1'>
-												<FaMapMarkerAlt className='mr-2 text-red-500' />
-												<h3 className='font-bold'>Localização: </h3>
-												<p>{field.location}</p>
-											</div>
+										<div className='text-gray-700 mb-2 flex items-center'>
+											<FaMapMarkerAlt className='mr-2 text-red-500' />
+											<h3 className='font-bold mr-1'>Localização:</h3>
+											<p>{field.location}</p>
 										</div>
 										<div className='text-gray-700 mb-2 flex items-center'>
-											<div className='flex items-center gap-1'>
-												<FaFutbol className='mr-2' />
-												<h3 className='font-bold'>Modalidade:</h3>
-												<span>{field.type}</span>
-											</div>
+											<FaFutbol className='mr-2' />
+											<h3 className='font-bold mr-1'>Modalidade:</h3>
+											<span>{field.type}</span>
 										</div>
 										<div className='text-gray-700 mb-2 flex items-center'>
-											<div className='flex items-center gap-1'>
-												<FaDollarSign className='mr-2 text-green-400' />
-												<h3 className='font-bold'>Valor da hora: </h3>
-												<span>R$ {field.hourly_rate}</span>
-											</div>
+											<FaDollarSign className='mr-2 text-green-400' />
+											<h3 className='font-bold mr-1'>Valor da hora:</h3>
+											<span>R$ {field.hourly_rate}</span>
 										</div>
 										<div className='h-48 mb-5'>
 											<Gallery
@@ -140,7 +175,7 @@ const HomePage = () => {
 																		data-pswp-width={intrinsicSize[image.path]?.width || 0}
 																		data-pswp-height={intrinsicSize[image.path]?.height || 0}
 																		onLoad={e => handleImageLoad(e, image.path)}
-																		className='h-full w-full object-cover rounded-lg max-h-48 cursor-pointer'
+																		className='h-full w-full object-cover rounded-lg cursor-pointer'
 																	/>
 																)}
 															</Item>
@@ -148,7 +183,7 @@ const HomePage = () => {
 													:	[
 															<div
 																className='h-full flex items-center justify-center'
-																key='no-images'
+																key='no-image'
 															>
 																<h3>Sem imagens</h3>
 															</div>,
@@ -158,36 +193,46 @@ const HomePage = () => {
 											</Gallery>
 										</div>
 
-										<div className='flex items-center '>
-											{user?.is_admin && (
-												<button
-													className='bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 transition duration-300 mt-3 mr-3 w-full'
-													onClick={() => handleEditClick(field)}
-												>
-													Editar
-												</button>
-											)}
+										<div className='flex items-center space-x-2'>
 											<button
-												className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 mt-3 w-full'
+												className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 w-full flex items-center justify-center'
 												onClick={() => handleRentClick(field)}
 											>
+												<FaShoppingCart className='mr-2' />
 												Alugar
 											</button>
+											{user?.is_admin && (
+												<>
+													<button
+														className='bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 transition duration-300 w-full flex items-center justify-center'
+														onClick={() => handleEditClick(field)}
+													>
+														<FaEdit className='mr-2' />
+														Editar
+													</button>
+													<button
+														className='bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 transition duration-300 w-full flex items-center justify-center'
+														onClick={() => handleDeleteClick(field)}
+													>
+														<FaTrash className='mr-2' />
+														Deletar
+													</button>
+												</>
+											)}
 										</div>
 									</div>
 								))}
 							</div>
-						:	<div className='flex justify-center'>
+						:	<div className='flex justify-center w-full'>
 								<h2>Nenhum arena disponível no momento.</h2>
 							</div>
 						}
 					</div>
-				:	<div className='flex justify-center items-center'>
+				:	<div className='flex justify-center items-center w-full'>
 						<h2>Carregando arenas...</h2>
 					</div>
 				}
 
-				{/* Modal para reserva */}
 				<Modal
 					title='Reserve seu horário'
 					open={isFieldDetailsModalVisible}
@@ -198,6 +243,16 @@ const HomePage = () => {
 				>
 					{selectedField && <FieldDetails field={selectedField} />}
 				</Modal>
+
+				<ConfirmationModal
+					isOpen={confirmationModal}
+					title='Confirmar Exclusão'
+					message='Tem certeza de que deseja excluir este campo?'
+					onConfirm={confirmDeleteField}
+					onCancel={() => setConfirmationModal(false)}
+					icon={<FaTrash size={16} />}
+					defaultClassName={true}
+				/>
 			</section>
 		</>
 	);
