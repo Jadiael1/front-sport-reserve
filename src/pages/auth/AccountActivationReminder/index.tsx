084753e2b2bc5689d713601509ac6e42cb08784e
Jadiael1 <jadiael@hotmail.com.br>
2024-07-30 20:04:50 -0300

    feat(auth): add resend section toggle in activation reminder

--- src/pages/auth/AccountActivationReminder/index.tsx
+++ src/pages/auth/AccountActivationReminder/index.tsx
@@ -7,6 +7,7 @@ const AccountActivationReminder = () => {
 	const [email, setEmail] = useState('');
 	const [message, setMessage] = useState<string | null>(null);
 	const [error, setError] = useState<string | null>(null);
+	const [showResendSection, setShowResendSection] = useState<boolean>(false);
 	const { user } = useAuth();
 	const navigate = useNavigate();
 	const baseURL = import.meta.env.VITE_API_BASE_URL;
@@ -57,27 +58,41 @@ const AccountActivationReminder = () => {
 				<h1 className='text-3xl font-bold mb-4'>Ative sua conta</h1>
 				<p className='text-lg text-gray-600 mb-4'>
 					Por favor, verifique seu email para encontrar o link de ativação da conta. Se você ainda não recebeu o email,
-					insira seu email abaixo e clique no botão para solicitar um novo link.
+					verifique sua caixa de entrada, spam ou lixo eletrônico. Se ainda não tiver recebido pessa novamente o link de
+					ativação da sua conta{' '}
+					<button
+						className='text-blue-600 hover:underline'
+						onClick={() => setShowResendSection(true)}
+					>
+						Cliccando Aqui!
+					</button>
 				</p>
-				<div className='mb-4'>
-					<label className='block text-gray-700 mb-2'>Email:</label>
-					<input
-						type='email'
-						name='email'
-						className='w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400'
-						required
-						value={email}
-						disabled={user?.email_verified_at !== null}
-						onChange={e => setEmail(e.target.value)}
-					/>
-				</div>
-				<button
-					disabled={user?.email_verified_at !== null}
-					onClick={handleResendActivationEmail}
-					className='w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300 disabled:bg-gray-400'
-				>
-					Reenviar email de ativação
-				</button>
+				{showResendSection && (
+					<>
+						<p className='text-lg text-gray-600 mb-4'>
+							Insira seu email abaixo e clique no botão para solicitar um novo link de ativação.
+						</p>
+						<div className='mb-4'>
+							<label className='block text-gray-700 mb-2'>Email:</label>
+							<input
+								type='email'
+								name='email'
+								className='w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400'
+								required
+								value={email}
+								disabled={user?.email_verified_at !== null}
+								onChange={e => setEmail(e.target.value)}
+							/>
+						</div>
+						<button
+							disabled={user?.email_verified_at !== null}
+							onClick={handleResendActivationEmail}
+							className='w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300 disabled:bg-gray-400'
+						>
+							Reenviar email de ativação
+						</button>
+					</>
+				)}
 				{message && (
 					<div className='mt-4 flex items-center justify-center text-green-500'>
 						<FaCheckCircle className='mr-2' />
