2f2ab625bbbab17a458839fefcbf28f27cf3f049
Jadiael <jadiael@hotmail.com.br>
2024-12-17 11:53:35 -0300

    Remove Google Maps API integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import { AuthProvider } from './contexts/AuthContext';
 import AppRoutes from './routes';
 import './assets/main.css';
-import { APIProvider } from '@vis.gl/react-google-maps';
-const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
 const App: React.FC = () => {
 	return (
 		<AuthProvider>
-			<APIProvider
-				apiKey={googleMapsApiKey}
-				language='pt'
-				region='BR'
-				libraries={['maps']}
-			>
-				<AppRoutes />
-			</APIProvider>
+			<AppRoutes />
 		</AuthProvider>
 	);
 };
