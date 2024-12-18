bb5f63a2f8829054ad8e59ac380ef4fa4a8848b8
Jadiael <jadiael@hotmail.com.br>
2024-12-18 17:07:39 -0300

    Fix button label typo from "Canclear" to "Cancelar"

--- src/pages/ReservationList/index.tsx
+++ src/pages/ReservationList/index.tsx
@@ -299,7 +299,7 @@ const ReservationList = () => {
 													className='mt-2 w-full bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 transition duration-300'
 													onClick={() => handleDelete(reservation.id)}
 												>
-													Canclear
+													Cancelar
 												</button>
 											</div>
 										}
